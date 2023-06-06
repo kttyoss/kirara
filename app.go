7dacb5dfe6d18240be3bc7f0bdca3517ff30f9c5
embed <brydon@helium.email>
2023-06-05 20:36:55 -0400

    [+] whitelist

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
@@ -22,10 +23,52 @@ func main() {
 	if strings.ToLower(os.Getenv("DEV_MODE")) == "true" {
 		color.Yellow("⚠ DEV_MODE is set to true. The whitelist has been disabled. This is not recommended in production and can lead to data leaks.")
 	}
+	// os.Getenv("WHITELIST_API") is the URL of the whitelist API. It will return the following JSON structure:
+	// {
+	// 	"success": true,
+	// 	"data": [
+	//     {
+	//		"name": "node name",
+	// 		"ip": "95.216.139.165:91237"
+	//      ...
+	//     }
+	//  ]
+	// }
+	// pull from this api and save all the ips (without port) in an array
+
+	whitelist := make(map[string]bool)
+
+	if strings.ToLower(os.Getenv("DEV_MODE")) != "true" {
+		resp, err := http.Get(os.Getenv("WHITELIST_API"))
+		if err != nil {
+			color.Red("⚠ Error while getting whitelist from API: %s", err.Error())
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		var jsonData map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&jsonData); err != nil {
+			color.Red("⚠ Error while decoding whitelist from API: %s", err.Error())
+			os.Exit(1)
+		}
+		if jsonData["success"] == false {
+			color.Red("⚠ Error while getting whitelist from API: %s", jsonData["message"].(string))
+			os.Exit(1)
+		}
+		for _, node := range jsonData["data"].([]interface{}) {
+			// whitelist[node.(map[string]interface{})["ip"].(string)] = true
+			whitelist[strings.Split(node.(map[string]interface{})["ip"].(string), ":")[0]] = true
+		}
+		if os.Getenv("DEV_MODE") == "append" {
+			whitelist["127.0.0.1"] = true
+		}
+	}
+
+	log.Printf("allowed ips: %v", whitelist)
+
 	app := fiber.New(fiber.Config{
-		Prefork:      true,
+		Prefork:      os.Getenv("PREFORK") == "true",
 		ServerHeader: "ktty kirara",
-		AppName:      "kirara node synchronizerq",
+		AppName:      "kirara node synchronizer",
 		JSONEncoder:  json.Marshal,
 		JSONDecoder:  json.Unmarshal,
 	})
@@ -33,7 +76,14 @@ func main() {
 	app.Static("/", "./www/index.html")
 
 	app.Use(func(c *fiber.Ctx) error {
+		if strings.ToLower(os.Getenv("DEV_MODE")) != "true" {
+			if _, ok := whitelist[c.IP()]; !ok {
+				log.Printf("unauthorized access from %s", c.IP())
+				return c.Status(fiber.StatusForbidden).SendString("You are not allowed to access this server.")
+			}
+		}
 		if websocket.IsWebSocketUpgrade(c) {
+			log.Printf("websocket connection from %s", c.IP())
 			return c.Next()
 		}
 		return c.SendStatus(fiber.StatusUpgradeRequired)
